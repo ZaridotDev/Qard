cee15192d3ea9e88b75626787a1a97800f7e12cf
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2026-02-27 17:35:32 -0300

    feat: restructuring credit expense list

--- src/ui/screens/HomeStack/CreditExpensesScreen.tsx
+++ src/ui/screens/HomeStack/CreditExpensesScreen.tsx
@@ -1,11 +1,10 @@
-import { View, Text, FlatList, Button, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { useMonthlyTransactions } from '../../../hooks/useMonthlyTransactions';
 import { formatDateForUI } from '../../../utils/dateFormatUI';
 import { getMonthRange } from '../../../utils/date';
-import { Trash, SquarePen, ChevronLeft, ChevronRight } from 'lucide-react-native';
+import { Trash, SquarePen, Square, SquareCheckBig } from 'lucide-react-native';
 import { useEffect, useState } from 'react';
 import { transactionService } from '../../../services/src/services/transactions.service';
-import { SelectMonthScreen } from './SelectMonthScreen';
 
 type CreditExpensesScreenType = {
   refreshTrigger?: number;
@@ -16,6 +15,8 @@ export function CreditExpensesScreen({ refreshTrigger = 0, crud }: CreditExpense
   const { startCurrentMonth, endCurrentMonth } = getMonthRange(new Date());
   const [selectMonth, setSelectMonth] = useState<string[]>([startCurrentMonth, endCurrentMonth]);
   const [ balance, setBalance ] = useState<number>(0);
+  const [ paid, setPaid ] = useState(false);
+
 
   const { transactions, loading, error } = useMonthlyTransactions(
     selectMonth[0],
@@ -61,6 +62,7 @@ export function CreditExpensesScreen({ refreshTrigger = 0, crud }: CreditExpense
       backgroundColor: 'trasnsparent',
       borderRadius: 16,
       marginBottom: 32,
+      flex: 1
       }}>
     {/* Container General */}
 
@@ -76,13 +78,14 @@ export function CreditExpensesScreen({ refreshTrigger = 0, crud }: CreditExpense
       {/* Titulos */}
 
       {/* Sombras */}
-      <View style={{elevation: 7, backgroundColor: "rgba(0,0,0,0.2)", position: 'absolute', height: '50%', width:100, zIndex: -1, borderTopStartRadius: 10, borderTopEndRadius: 10}}/>
-      <View style={{elevation: 7, backgroundColor: "rgba(0,0,0,0.2)", position: 'absolute', height: '50%', width:150, zIndex: -1, borderTopStartRadius: 10, borderTopEndRadius: 10, right: 0}}/>
-      <View style={{elevation: 7, backgroundColor: "rgba(0,0,0,0.2)", position: 'absolute', height: '50%', width:'100%', zIndex: -1, borderBottomStartRadius: 10, borderBottomEndRadius: 10 , bottom: 0}}/>
+      <View style={{elevation: 7, backgroundColor: "rgba(0,0,0,0.2)", position: 'absolute', height: 40, width:100, zIndex: -1, borderTopStartRadius: 10, borderTopEndRadius: 10}}/>
+      <View style={{elevation: 7, backgroundColor: "rgba(0,0,0,0.2)", position: 'absolute', height: 40, width:150, zIndex: -1, borderTopStartRadius: 10, borderTopEndRadius: 10, right: 0}}/>
+      <View style={{elevation: 7, backgroundColor: "rgba(0,0,0,0.2)", position: 'absolute', top: 40, bottom: 35, right: 0, left: 0, zIndex: -1, borderBottomStartRadius: 10, }}/>
+      <View style={{elevation: 7, backgroundColor: "rgba(0,0,0,0.2)", position: 'absolute', height: 35, width: 110, zIndex: -1, borderBottomStartRadius: 10, borderBottomEndRadius: 10 , bottom: 0, right: 0}}/>
       {/* Sombras */}
 
       {/* Card de egresos */}
-      <View style={{ backgroundColor: '#D9E7CB', minHeight: 100, borderBottomStartRadius: 10, borderBottomEndRadius: 10, padding: 4}}>
+      <View style={{ backgroundColor: '#D9E7CB', minHeight: 100, borderBottomStartRadius: 10, padding: 4}}>
 
         {/* <View style={{ backgroundColor: 'rgba(0,0,0,0.1)',  height: '97%', position: 'absolute', top: 5, right: 55, width: 2, zIndex: 1}}/> */}
         <View style={{ backgroundColor: 'rgba(255, 255, 255, 0.2)',  height: '97%', position: 'absolute', top: 5, right: 93, width: 2, zIndex: 1}}/>
@@ -105,21 +108,38 @@ export function CreditExpensesScreen({ refreshTrigger = 0, crud }: CreditExpense
                   backgroundColor: getTransactionType(item.type) ? '#BAD3A2' : '#E7B8B8', 
                 }}
               >
-              <Text style={{ fontSize: 14, flex: 3, marginLeft: 12 }}>{item.description}</Text>
+              <View style={{ flex: 3, marginLeft: 12, flexDirection: 'row', }}>
+                <Text style={{ fontSize: 14 }}>{item.description}</Text>
+                <Text style={{ fontSize: 12, fontWeight: 'bold', textAlign: 'right', marginHorizontal: 4}}>{/* Cuotas */} 1/6</Text>
+              </View>
               <Text style={{ fontSize: 14, flex: 2, textAlign: 'right', marginHorizontal: 4 }}>{item.amount}</Text>
-              <Text style={{ fontSize: 12, fontWeight: 'bold', flex: 0.7, textAlign: 'right', marginHorizontal: 4, width: 10 }}>{formatDateForUI(item.transaction_date)}</Text>
+
               <TouchableOpacity style={{ marginLeft: 4, }}>
                 <SquarePen size={16} />
               </TouchableOpacity>
-              <TouchableOpacity style={{ marginHorizontal: 8, }} onPress={() => deleteItem(item.id)}>
+              <TouchableOpacity style={{ marginLeft: 4, }} onPress={() => deleteItem(item.id)}>
                 <Trash size={16} />
+              </TouchableOpacity>
+              <TouchableOpacity style={{ marginHorizontal: 8, }} onPress={() => setPaid(true)}>
+                {!paid 
+                ? <Square size={16} />
+                : <SquareCheckBig size={16} />
+                }
               </TouchableOpacity>
             </View>
           )}
           /> 
       </View>
       {/* Card de egresos */}
-
+      <View style={{ backgroundColor: '#D9E7CB', width: 110, borderBottomStartRadius: 10, borderBottomEndRadius: 10, padding: 8, alignSelf: 'flex-end', alignItems: 'center', flexDirection: 'row'}}>
+        <Text style={{ fontSize: 12, textAlign: 'right', marginHorizontal: 4 }}>Pagar total: </Text>    
+        <TouchableOpacity  onPress={() => setPaid(true)}>
+          {!paid 
+          ? <Square size={18} />
+          : <SquareCheckBig size={18} />
+          }
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
