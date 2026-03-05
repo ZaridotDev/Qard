5efad03ab66891c35fae329373ffae5079e2c669
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2026-03-05 00:06:35 -0300

    feat: fixing ModalIncome UI

--- src/ui/components/Modals/ModalIncome.tsx
+++ src/ui/components/Modals/ModalIncome.tsx
@@ -1,8 +1,9 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, Button, Modal, TextInput } from "react-native";
+import { View, Text, Button, Modal, TextInput, TouchableOpacity } from "react-native";
 import { transactionService } from "../../../services/src/services/transactions.service";
 import { getMonthRange } from "../../../utils/date";
 import { useState } from "react";
+import { X } from "lucide-react-native";
 
 type ModalIncomeType = {
     visible: boolean;
@@ -49,32 +50,43 @@ export function ModalIncome ({visible, onClose}: ModalIncomeType) {
         <Modal
             visible={visible}
             animationType='fade'
-            backdropColor={'rgba(0, 0, 0, 0.3)'}
+            backdropColor={'rgba(0, 0, 0, 0.1)'}
             onRequestClose={() => console.log('new Income')}
-            // style={{flex: 1, justifyContent: 'center', alignContent: 'center'}}
         >
             <View style={{flex: 1, justifyContent: 'center', padding: 20}}>
-                <View style={{alignSelf: 'center', justifyContent: 'space-between',width: '80%', height: '40%', backgroundColor: '#BAD3A2', padding: 20, borderRadius: 15, alignItems: 'center'}}>
-                    <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>Descripcion</Text>
+                <View style={{alignSelf: 'center', width: '80%', height: 260, backgroundColor: '#BAD3A2', padding: 20, borderRadius: 15, alignItems: 'center'}}>
+                    <TouchableOpacity 
+                    onPress={() => {
+                        setTeton('');
+                        setDescription('');
+                        onClose(false)}} 
+                    style={{ padding: 10, position: 'absolute', top: 5, right:5 }}
+                    >
+                        <X size={24} color={'white'} />
+                    </TouchableOpacity>
+
+                    <Text style={{color: 'white', fontSize: 22, marginBottom: 4, fontWeight: 'bold'}}>Descripcion</Text>
                     <TextInput 
-                        style={{width: '100%', backgroundColor: 'white', height: 40, textAlign: 'center', fontSize: 18 }}
+                        style={{width: '100%', backgroundColor: 'white', height: 40, textAlign: 'center', fontSize: 18, marginBottom: 15 }}
                         placeholder="Sueldo"
                         value={description}
                         onChangeText={(text) => setDescription(text)}
                     />
-                    <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>Monto</Text>
+                    <Text style={{color: 'white', fontSize: 22, marginBottom: 4, fontWeight: 'bold'}}>Monto</Text>
                     <TextInput 
-                        style={{width: '100%', backgroundColor: 'white', height: 40, textAlign: 'center', fontSize: 18 }}
+                        style={{width: '100%', backgroundColor: 'white', height: 40, textAlign: 'center', fontSize: 18, marginBottom: 15 }}
                         placeholder="$1000"
                         value={teton}
                         onChangeText={(text) => setTeton(text)}
                         
                     />
-                    <Button onPress={() => createIncome()} title='Aceptar' color={'#5C7E3B'} />
-                    <Button onPress={() => {
-                        setTeton('');
-                        setDescription('');
-                        onClose(false)}} title='Cancelar' color={'#5C7E3B'} />
+                    <TouchableOpacity 
+                    onPress={() => createIncome()} 
+                    >
+                        <View style={{backgroundColor: '#5C7E3B', padding: 10, borderRadius: 10,}}>
+                            <Text style={{color: 'white', fontSize: 20}}>Aceptar</Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
             </View>
         </Modal>
