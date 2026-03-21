968326554fd8de131103e72eb135edfb847f2c02
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2026-03-21 13:42:50 -0300

    style: style changes in the Drawer

--- src/navigation/HamburgerMenu.tsx
+++ src/navigation/HamburgerMenu.tsx
@@ -5,6 +5,9 @@ import { SessionScreen } from '../ui/screens/SessionStack/SessionScreen';
 import { HomeStack } from './HomeStack';
 import { AuthScreen } from '../ui/screens/SessionStack/AuthScreen';
 import { useAuth } from '../hooks/useAuth';
+import { UserRound } from 'lucide-react-native';
+import { colors } from '../ui/styles/colors';
+import { View } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
@@ -15,10 +18,48 @@ export function HamburgerMenu() {
 
   if (!isAuthenticated) return <AuthScreen />;
   return (
-    <Drawer.Navigator screenOptions={{ headerShown: false,  }}>
-      <Drawer.Screen name="HomeStack" component={HomeStack} />
-      <Drawer.Screen name="Analytics" component={AnalyticsScreen} />
-      <Drawer.Screen name="Session" component={SessionScreen} />
+    <Drawer.Navigator 
+      initialRouteName="HomeStack"
+      screenOptions={{ 
+        headerShown: false,  
+        drawerStyle: {
+          backgroundColor: colors[3],
+          width: 280
+        },
+        drawerLabelStyle:{
+          fontSize: 16,
+          color: '#333',
+        },
+        drawerActiveBackgroundColor: colors[5],
+        // drawerActiveTintColor: colors[1],
+        // drawerInactiveTintColor: '#ccc',
+      }}
+    >
+      <Drawer.Screen 
+        name="Session" 
+        component={SessionScreen} 
+        options={{ 
+          title: 'Configuración',
+          drawerIcon: ({ focused }) => (
+            <View
+              style={{
+                // width: 42,
+                // height: 42,
+                // borderRadius: 21,
+                // justifyContent: 'center',
+                // alignItems: 'center',
+                backgroundColor: focused ? colors[2] : colors[1],
+                borderRadius: 50,
+                padding: 5
+                }}
+            >
+              <UserRound size={42} color={focused ? colors[6] : colors[4]} />
+            </View>
+          ),
+        }}
+      />
+      <Drawer.Screen name="HomeStack" component={HomeStack} options={{ title: 'Página principal'}}/>
+      <Drawer.Screen name="Analytics" component={AnalyticsScreen} options={{ title: 'Estadísticas'}}/>
     </Drawer.Navigator>
   );
 }
--- src/ui/components/DrawerButon.tsx
+++ src/ui/components/DrawerButon.tsx
@@ -1,9 +1,10 @@
 import { DrawerToggleButton } from "@react-navigation/drawer";
 import { View } from "react-native";
+import { colors } from "../styles/colors";
 
 export function DrawerButon () {
     return (
-        <View style={{ backgroundColor: '#ecf4e4', position: 'absolute', borderRadius: 10, top: 20, left: 5}}>
+        <View style={{ backgroundColor: colors[3], position: 'absolute', borderRadius: 20, top: 5, left: 10, paddingVertical: 4}}>
             <DrawerToggleButton /> {/* ReBambis */}
         </View>
     )
