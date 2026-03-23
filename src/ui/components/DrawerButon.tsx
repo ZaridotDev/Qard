052a897e1ce892acaeaf3a111a79b39643d5c96f
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2026-03-23 17:24:15 -0300

    build: settings for creating APKs

--- src/ui/components/DrawerButon.tsx
+++ src/ui/components/DrawerButon.tsx
@@ -4,7 +4,16 @@ import { colors } from "../styles/colors";
 
 export function DrawerButon () {
     return (
-        <View style={{ backgroundColor: colors[3], position: 'absolute', borderRadius: 20, top: 5, left: 10, paddingVertical: 4}}>
+        <View 
+            style={{ 
+                backgroundColor: colors[3], 
+                position: 'absolute', 
+                borderRadius: 20, 
+                top: 5, 
+                left: 10,
+                paddingVertical: 4
+            }}
+        >
             <DrawerToggleButton /> {/* ReBambis */}
         </View>
     )
